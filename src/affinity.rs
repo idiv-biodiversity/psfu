9e4476b19a91c90932d24d0d5808f7c09e7e4907
Christian Krause <christian.krause@idiv.de>
2022-12-07 12:10:13 +0100

    avoids MaybeUnit with mem::zereod

--- src/affinity.rs
+++ src/affinity.rs
@@ -1,18 +1,15 @@
 //! Getting and setting processor affinity aka cpuset.
 
-use std::mem::{self, MaybeUninit};
-
 use anyhow::{anyhow, Result};
 use errno::errno;
 
 // For the joy of using a C interface.
-static CPU_SET_SIZE: usize = mem::size_of::<libc::cpu_set_t>();
+static CPU_SET_SIZE: usize = std::mem::size_of::<libc::cpu_set_t>();
 
 /// Returns the processor affinity aka cpuset.
 pub fn get(pid: libc::pid_t) -> Result<Vec<usize>> {
     let mask = unsafe {
-        #[allow(clippy::uninit_assumed_init)]
-        let mut mask = MaybeUninit::uninit().assume_init();
+        let mut mask = std::mem::zeroed();
         libc::CPU_ZERO(&mut mask);
 
         if 0 != libc::sched_getaffinity(pid, CPU_SET_SIZE, &mut mask) {
@@ -36,8 +33,7 @@ pub fn get(pid: libc::pid_t) -> Result<Vec<usize>> {
 /// Sets processor affinity aka cpuset.
 pub fn set(pid: libc::pid_t, cpuset: &[usize]) -> Result<()> {
     unsafe {
-        #[allow(clippy::uninit_assumed_init)]
-        let mut mask = MaybeUninit::uninit().assume_init();
+        let mut mask = std::mem::zeroed();
         libc::CPU_ZERO(&mut mask);
 
         for i in cpuset {
