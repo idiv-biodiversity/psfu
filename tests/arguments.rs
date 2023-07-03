31ba115c867d3dc9e8d66e4a1ee52627f4f6f37d
Christian Krause <christian.krause@idiv.de>
2023-07-03 15:24:53 +0200

    fixes testing on case sensitive error messages

--- tests/arguments.rs
+++ tests/arguments.rs
@@ -10,17 +10,19 @@ fn pid() -> Result<(), Box<dyn Error>> {
     let mut cmd = util::bin(&["tree", "show", "plain", "x"])?;
     cmd.assert()
         .failure()
-        .stderr(predicate::str::contains("error: Invalid value"));
+        .stderr(predicate::str::is_match("error: [Ii]nvalid value").unwrap());
 
     let mut cmd = util::bin(&["tree", "show", "plain", "0"])?;
     cmd.assert()
         .failure()
-        .stderr(predicate::str::contains("error: Invalid value"));
+        .stderr(predicate::str::is_match("error: [Ii]nvalid value").unwrap());
 
     let mut cmd = util::bin(&["tree", "show", "plain", "1"])?;
-    cmd.assert()
-        .success()
-        .stderr(predicate::str::contains("error: Invalid value").not());
+    cmd.assert().success().stderr(
+        predicate::str::is_match("error: [Ii]nvalid value")
+            .unwrap()
+            .not(),
+    );
 
     let pid_max = procfs::sys::kernel::pid_max()?;
     let too_high = format!("{}", pid_max + 1);
@@ -28,7 +30,7 @@ fn pid() -> Result<(), Box<dyn Error>> {
     let mut cmd = util::bin(&["tree", "show", "plain", &too_high])?;
     cmd.assert()
         .failure()
-        .stderr(predicate::str::contains("error: Invalid value"));
+        .stderr(predicate::str::is_match("error: [Ii]nvalid value").unwrap());
 
     Ok(())
 }
