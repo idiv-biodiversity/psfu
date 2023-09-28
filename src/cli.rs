ced533b169a502db80cc6e3243fdccbbfb458d55
Christian Krause <christian.krause@idiv.de>
2023-09-28 03:26:47 +0200

    [cli/internal] reorganize module into smaller functions

--- src/cli.rs
+++ src/cli.rs
@@ -3,131 +3,10 @@ use std::io::{stdin, IsTerminal};
 use clap::{crate_description, crate_name, crate_version};
 use clap::{Arg, ArgAction, Command};
 
+pub const SUBCOMMAND_REQUIRED: &str =
+    "CLI argument parser should have been set up to require a subcommand";
+
 pub fn build() -> Command {
-    // ------------------------------------------------------------------------
-    // arguments
-    // ------------------------------------------------------------------------
-
-    let pid = Arg::new("pid")
-        .help("process IDs")
-        .action(ArgAction::Append)
-        .required(stdin().is_terminal())
-        .value_parser(is_pid);
-
-    let arguments = Arg::new("arguments")
-        .long("arguments")
-        .short('a')
-        .action(ArgAction::SetTrue)
-        .help("show arguments");
-
-    let cpuset = Arg::new("cpuset")
-        .help("single integer or 'free' for all")
-        .required(true)
-        .action(ArgAction::Set)
-        .value_parser(is_cpuset);
-
-    let threads = Arg::new("threads")
-        .long("threads")
-        .short('t')
-        .action(ArgAction::SetTrue)
-        .help("include threads");
-
-    let verbose = Arg::new("verbose")
-        .long("verbose")
-        .action(ArgAction::SetTrue)
-        .long_help("Adds verbose output.")
-        .hide_short_help(true);
-
-    // ------------------------------------------------------------------------
-    // help/version modifications
-    // ------------------------------------------------------------------------
-
-    let help = Arg::new("help")
-        .short('?')
-        .long("help")
-        .action(ArgAction::Help)
-        .help("print help (use --help to see all options)")
-        .long_help("Print help.");
-
-    let version = Arg::new("version")
-        .long("version")
-        .help("print version")
-        .long_help("Print version.")
-        .action(ArgAction::Version);
-
-    // ------------------------------------------------------------------------
-    // show commands
-    // ------------------------------------------------------------------------
-
-    let plain = Command::new("plain")
-        .arg(&help)
-        .arg(&pid)
-        .arg(&arguments)
-        .arg(&threads)
-        .about("show process tree");
-
-    let affinity_s = Command::new("affinity")
-        .arg(&help)
-        .arg(&pid)
-        .arg(&threads)
-        .about("show process tree with affinity (cpuset)");
-
-    let backtrace = Command::new("backtrace")
-        .alias("bt")
-        .arg(&help)
-        .arg(&pid)
-        .arg(&threads)
-        .arg(&verbose)
-        .about("show process tree with backtrace");
-
-    // ------------------------------------------------------------------------
-    // modify commands
-    // ------------------------------------------------------------------------
-
-    let affinity_m = Command::new("affinity")
-        .arg(&cpuset)
-        .arg(&help)
-        .arg(&pid)
-        .arg(&threads)
-        .arg(&verbose)
-        .about("modify process tree affinity (cpuset)");
-
-    // ------------------------------------------------------------------------
-    // tree commands
-    // ------------------------------------------------------------------------
-
-    let modify = Command::new("modify")
-        .about("modify processes")
-        .arg_required_else_help(true)
-        .disable_help_flag(true)
-        .subcommand_required(true)
-        .subcommand(affinity_m);
-
-    let show = Command::new("show")
-        .about("show processes")
-        .arg_required_else_help(true)
-        .disable_help_flag(true)
-        .subcommand_required(true)
-        .subcommand(affinity_s)
-        .subcommand(backtrace)
-        .subcommand(plain);
-
-    // ------------------------------------------------------------------------
-    // top-level commands
-    // ------------------------------------------------------------------------
-
-    let tree = Command::new("tree")
-        .about("process tree commands")
-        .arg_required_else_help(true)
-        .disable_help_flag(true)
-        .subcommand_required(true)
-        .subcommand(modify)
-        .subcommand(show);
-
-    // ------------------------------------------------------------------------
-    // put it all together
-    // ------------------------------------------------------------------------
-
     Command::new(crate_name!())
         .about(crate_description!())
         .version(crate_version!())
@@ -137,10 +16,150 @@ pub fn build() -> Command {
         .disable_version_flag(true)
         .infer_subcommands(true)
         .subcommand_required(true)
-        .subcommand(tree)
-        .arg(help)
-        .arg(version)
+        .subcommand(cmd_tree())
+        .arg(arg_help())
+        .arg(arg_version())
 }
+
+// ----------------------------------------------------------------------------
+// commands
+// ----------------------------------------------------------------------------
+
+fn cmd_tree() -> Command {
+    Command::new("tree")
+        .about("process tree commands")
+        .arg_required_else_help(true)
+        .disable_help_flag(true)
+        .subcommand_required(true)
+        .subcommand(cmd_tree_modify())
+        .subcommand(cmd_tree_show())
+}
+
+fn cmd_tree_modify() -> Command {
+    Command::new("modify")
+        .about("modify processes")
+        .arg_required_else_help(true)
+        .disable_help_flag(true)
+        .subcommand_required(true)
+        .subcommand(cmd_modify_affinity())
+}
+
+fn cmd_tree_show() -> Command {
+    Command::new("show")
+        .about("show processes")
+        .arg_required_else_help(true)
+        .disable_help_flag(true)
+        .subcommand_required(true)
+        .subcommand(cmd_show_affinity())
+        .subcommand(cmd_show_backtrace())
+        .subcommand(cmd_show_plain())
+}
+
+// ----------------------------------------------------------------------------
+// leaf commands
+// ----------------------------------------------------------------------------
+
+fn cmd_modify_affinity() -> Command {
+    Command::new("affinity")
+        .arg(arg_cpuset())
+        .arg(arg_help())
+        .arg(arg_pid())
+        .arg(arg_threads())
+        .arg(arg_verbose())
+        .about("modify process tree affinity (cpuset)")
+}
+
+fn cmd_show_affinity() -> Command {
+    Command::new("affinity")
+        .arg(arg_help())
+        .arg(arg_pid())
+        .arg(arg_threads())
+        .about("show process tree with affinity (cpuset)")
+}
+
+fn cmd_show_backtrace() -> Command {
+    Command::new("backtrace")
+        .alias("bt")
+        .arg(arg_help())
+        .arg(arg_pid())
+        .arg(arg_threads())
+        .arg(arg_verbose())
+        .about("show process tree with backtrace")
+}
+
+fn cmd_show_plain() -> Command {
+    Command::new("plain")
+        .arg(arg_help())
+        .arg(arg_pid())
+        .arg(arg_show_arguments())
+        .arg(arg_threads())
+        .about("show process tree")
+}
+
+// ----------------------------------------------------------------------------
+// arguments
+// ----------------------------------------------------------------------------
+
+fn arg_cpuset() -> Arg {
+    Arg::new("cpuset")
+        .help("single integer or 'free' for all")
+        .required(true)
+        .action(ArgAction::Set)
+        .value_parser(is_cpuset)
+}
+
+fn arg_help() -> Arg {
+    Arg::new("help")
+        .short('?')
+        .long("help")
+        .action(ArgAction::Help)
+        .help("print help (use --help to see all options)")
+        .long_help("Print help.")
+}
+
+fn arg_pid() -> Arg {
+    Arg::new("pid")
+        .help("process IDs")
+        .action(ArgAction::Append)
+        .required(stdin().is_terminal())
+        .value_parser(is_pid)
+}
+
+fn arg_show_arguments() -> Arg {
+    Arg::new("arguments")
+        .long("arguments")
+        .short('a')
+        .action(ArgAction::SetTrue)
+        .help("show arguments")
+}
+
+fn arg_threads() -> Arg {
+    Arg::new("threads")
+        .long("threads")
+        .short('t')
+        .action(ArgAction::SetTrue)
+        .help("include threads")
+}
+
+fn arg_verbose() -> Arg {
+    Arg::new("verbose")
+        .long("verbose")
+        .action(ArgAction::SetTrue)
+        .long_help("Adds verbose output.")
+        .hide_short_help(true)
+}
+
+fn arg_version() -> Arg {
+    Arg::new("version")
+        .long("version")
+        .help("print version")
+        .long_help("Print version.")
+        .action(ArgAction::Version)
+}
+
+// ----------------------------------------------------------------------------
+// value parsers
+// ----------------------------------------------------------------------------
 
 fn is_cpuset(s: &str) -> Result<String, String> {
     if s == "free" || s.parse::<u64>().is_ok() {
@@ -153,9 +172,6 @@ fn is_cpuset(s: &str) -> Result<String, String> {
 fn is_pid(s: &str) -> Result<i32, String> {
     crate::pid::validate(s)
 }
-
-pub const SUBCOMMAND_REQUIRED: &str =
-    "CLI argument parser should have been set up to require a subcommand";
 
 // ----------------------------------------------------------------------------
 // tests
